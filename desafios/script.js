5adcfb5f9893ee223d0d74ff9814a6ce9d6ba43b
Adriano G <adrianogomesfilho98@gmail.com>
2023-08-08 20:24:11 -0300

    .

--- desafios/script.js
+++ desafios/script.js
@@ -1,9 +1,15 @@
-const lista = document.querySelector(".lista");
 
-function mostraTintas(this) {
-  lista.addEventListener ("click", function (evento) {
-    evento.target
-  }
-  
-  
+const aparece = "aparece"
+const some = "some"
+
+function mostraTintas(variavel) {
+    if(variavel === "aparece") {
+      console.log("peidei")
+      document.querySelector(".lista").style.display = "block";
+
+    } else {
+      console.log("some")
+      document.querySelector(".lista").style.display = "none";
+
+    }
 }
